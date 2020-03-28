55b32cb72e3053e0f522d2c47f13d60760b9f738
osama <ahm_osama@yahoo.com>
2020-03-28 17:35:24 +0600

    Service tutorial

--- app/src/main/java/com/example/osama/tutorialday1/MainActivity.java
+++ app/src/main/java/com/example/osama/tutorialday1/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.osama.tutorialday1;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    String msg ="Android : ";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate()event");
     }
+
+    public void startService(View view){
+        startService(new Intent(MainActivity.this,MyService.class));
+
+
+    }
+
+    public void stopService(View view) {
+        stopService(new Intent(MainActivity.this,MyService.class));
+    }
+
 }
