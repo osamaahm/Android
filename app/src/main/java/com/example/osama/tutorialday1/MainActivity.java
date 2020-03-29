889aa4f4f75374beed07445e250d9fc3d30c112f
osama <ahm_osama@yahoo.com>
2020-03-29 18:19:32 +0600

    Content Provider (SQLite)
    parent :Broadcast Receiver (custom)
            Broadcast Receiver (system)
            service

--- app/src/main/java/com/example/osama/tutorialday1/MainActivity.java
+++ app/src/main/java/com/example/osama/tutorialday1/MainActivity.java
@@ -1,37 +1,76 @@
 package com.example.osama.tutorialday1;
 
 import android.content.ComponentName;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    String msg ="Android : ";
+    String msg = "Android : ";
+
+    EditText nameET;
+    EditText gradeET;
+    Button addBtn;
+    Button retriveBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(msg, "The onCreate()event");
+        /*nameET = (EditText) findViewById(R.id.editTxt1);
+        gradeET = (EditText) findViewById(R.id.editTxt2);
+        addBtn = (Button) findViewById(R.id.addbtn);
+        retriveBtn = (Button) findViewById(R.id.retriveBtn);*/
     }
 
-    public void startService(View view){
-        startService(new Intent(MainActivity.this,MyService.class));
+    public void startService(View view) {
+        startService(new Intent(MainActivity.this, MyService.class));
 
 
     }
 
     public void stopService(View view) {
-        stopService(new Intent(MainActivity.this,MyService.class));
+        stopService(new Intent(MainActivity.this, MyService.class));
     }
-    public void broadcastIntent(View view)
-    {
+
+    public void broadcastIntent(View view) {
         Intent intent = new Intent();
         intent.setAction("com.osama.CUSTOM_INTENT");
         sendBroadcast(intent);
+    }
 
+    public void onClickAddName(View view){
+        ContentValues values = new ContentValues();
+        values.put(StudentsProvider.NAME,((EditText)findViewById(R.id.editTxt1)).getText().toString());
 
+        values.put(StudentsProvider.GRADE, ((EditText)findViewById(R.id.editTxt2)).getText().toString());
+
+        Uri uri = getContentResolver().insert(StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+    }
+    public void onClickRetrive(View view){
+        String URL = StudentsProvider.URL;
+        Uri students = Uri.parse(URL);
+
+        Cursor c = getContentResolver().query(students, null, null, null, "name");
+
+        if(c.moveToFirst()){
+            do{
+                Toast.makeText(this, c.getString(c.getColumnIndex(StudentsProvider._ID) )
+                        + " , " + c.getString(c.getColumnIndex(StudentsProvider.NAME)) + " , " +
+                        c.getString(c.getColumnIndex(StudentsProvider.GRADE)) ,
+                        Toast.LENGTH_SHORT).show();
+            }while (c.moveToNext());
+        }
     }
 
 }
